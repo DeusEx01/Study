c3358a06478d4ffa86e3fb57de042743b0375d7d
DeusEx01 <theonly.lightfall002@gmail.com>
2024-10-05 13:58:49 +0300

    repeating task

--- Asynchrony/task_1.js
+++ Asynchrony/task_1.js
@@ -1,44 +1,85 @@
-const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+// const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
-const dataContainer = document.querySelector("#data-container");
+// const dataContainer = document.querySelector("#data-container");
 
-const createUsers = (userName) => {
-  const userElement = document.createElement("li");
-  const userElementLink = document.createElement("a");
-  userElementLink.href = "#";
-  userElementLink.textContent = userName;
+// const createUsers = (userName) => {
+//   const userElement = document.createElement("li");
+//   const userElementLink = document.createElement("a");
+//   userElementLink.href = "#";
+//   userElementLink.textContent = userName;
 
-  userElement.append(userElementLink);
+//   userElement.append(userElementLink);
+//   return userElement;
+// };
+
+// const loader = function () {
+//   let loader = document.querySelector("#data-loader");
+//   loader.hidden = !loader.hidden;
+// };
+
+// const getUsers = () => {
+//   loader();
+//   let result = fetch(USERS_URL, { method: "GET" });
+
+//   setTimeout( () => {
+//     result
+//     .then((response) => {
+//       let users = response.json();
+//       return users;
+//     })
+//     .then((users) => {
+//       users.forEach((user) => {
+//         dataContainer.append(createUsers(user.name));
+//       });
+//     })
+//     .catch(error => {
+//       throw new Error('Something went wrong')
+//     })
+//     .finally(() => {
+//       loader();
+//     })
+//   }, 3000)
+// };
+
+// getUsers();
+
+
+function addUserToList(userName) {
+  const userElement = document.createElement('li');
+  const userAnchor = document.createElement('a');
+  userAnchor.href = '#';
+  userAnchor.textContent = userName;
+  userElement.append(userAnchor);
+
   return userElement;
-};
+}
 
-const loader = function () {
-  let loader = document.querySelector("#data-loader");
-  loader.hidden = !loader.hidden;
-};
+function loader() {
+  let loader = document.querySelector('#data-loader').hidden 
+  loader != loader;
+}
+const users_url = 'https://jsonplaceholder.typicode.com/users';
 
-const getUsers = () => {
+function getUsersList() {
   loader();
-  let result = fetch(USERS_URL, { method: "GET" });
+  const result = fetch(users_url);
 
-  setTimeout( () => {
-    result
-    .then((response) => {
-      let users = response.json();
-      return users;
+  result
+    .then( result => {
+      return result.json();
     })
-    .then((users) => {
-      users.forEach((user) => {
-        dataContainer.append(createUsers(user.name));
-      });
+    .then( (users) => {
+      const userList = document.querySelector('#data-container');
+      users.forEach( user => { //getting user's nickname and user's name 
+        userList.append(addUserToList(`${user.username} (${user.name})`));
+      })
     })
-    .catch(error => {
-      throw new Error('Something went wrong')
+    .catch( error => {
+      throw new Error(`Something went wrong ${error}`);
     })
-    .finally(() => {
+    .finally( () => {
       loader();
     })
-  }, 3000)
-};
+}
 
-getUsers();
+getUsersList()
