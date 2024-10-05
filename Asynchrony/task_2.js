0924cccd458bca4dd634577c825083bb3d670d8b
DeusEx01 <theonly.lightfall002@gmail.com>
2024-10-05 14:59:09 +0300

    update

---
+++ Asynchrony/task_2.js
@@ -0,0 +1,10 @@
+/*
+To create a function getUsersByIds, which will take array with users 'id'. You need to g et all users with these 'id'. Add name data into each html-element.
+Also add loader element before loading users and hide it after loading ones.
+
+Template for user html-elements:
+<li><a href="#">User name</a></li>
+
+For testing function user:
+getUsersByIds([5, 6, 2, 1])
+*/
