2b543a47936f7195eb8e6db7985e86fd149f822c
DeusEx01 <theonly.lightfall002@gmail.com>
2024-10-10 19:35:03 +0300

    update

--- Vue.js Crash Course 2024 by Traversy Media/src/App.vue
+++ Vue.js Crash Course 2024 by Traversy Media/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
 
 
 
 </script>
 <template>
   <NavBar />
+  <Hero/>
+  <HomeCards/>
 </template>
