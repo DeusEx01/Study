3d3e54d6eb36d60896a8582245e58c23573f8552
DeusEx01 <theonly.lightfall002@gmail.com>
2024-10-10 21:23:28 +0300

    added router and JobListings

--- Vue.js Crash Course 2024 by Traversy Media/src/components/HomeCards.vue
+++ Vue.js Crash Course 2024 by Traversy Media/src/components/HomeCards.vue
@@ -6,7 +6,7 @@ import Card from '@/components/Card.vue'
   <section class="py-4">
     <div class="container-xl lg:container m-auto">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-        <Card class="bg-gray-100 p-6 rounded-lg shadow-md">
+        <Card>
           <h2 class="text-2xl font-bold">For Developers</h2>
           <p class="mt-2 mb-4">
             Browse our Vue jobs and start your career today
@@ -15,7 +15,7 @@ import Card from '@/components/Card.vue'
             Browse Jobs
           </a>
         </Card>
-        <Card class="bg-green-100 p-6 rounded-lg shadow-md">
+        <Card bg="bg-green-100">
           <h2 class="text-2xl font-bold">For Employers</h2>
           <p class="mt-2 mb-4">
             List your job to find the perfect developer for the role
